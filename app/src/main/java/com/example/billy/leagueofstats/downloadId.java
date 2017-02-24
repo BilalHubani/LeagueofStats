cbaea26bd6aa5df0033249e966a16185877b9d17
BilalHubani <bilal.hubani@gmail.com>
2017-02-24 21:17:11 +0100

    spinner done

--- app/src/main/java/com/example/billy/leagueofstats/downloadId.java
+++ app/src/main/java/com/example/billy/leagueofstats/downloadId.java
@@ -3,21 +3,18 @@ package com.example.billy.leagueofstats;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.view.View;
-import android.widget.ProgressBar;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.StringReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 /**
  * Created by dam on 24/2/17.
  */
-
 public class DownloadId extends AsyncTask<Void, String, String> {
     private Context context;
     private OnInfoLoadedListener listener = null;
@@ -36,24 +33,25 @@ public class DownloadId extends AsyncTask<Void, String, String> {
         BufferedReader reader = null;
 
         try {
-
+            //fasdf asdf sad
             URL url = new URL("http://stucom.flx.cat/lego/get_set_parts.php?set=60115-1&key=62fb8715af2c04f5d9a3d69bdde21e65");
-            URLConnection connection = url.openConnection();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.connect();
             int lengthOfFile = connection.getContentLength();
+            int responseCode = connection.getResponseCode();
             Log.e("funciona", "hasta aqui");
-            InputStream input = new BufferedInputStream(url.openStream(), 8192);
-            ByteArrayOutputStream output = new ByteArrayOutputStream();
-            byte data[] = new byte[1024];
-            long total = 0;
-            while ((count = input.read(data)) != -1) {
-                total += count;
-                publishProgress("" + (int) ((total * 100) / lengthOfFile));
-                output.write(data, 0, count);
+            if (responseCode == 200) {
+                InputStream input = new BufferedInputStream(url.openStream(), 8192);
+                ByteArrayOutputStream output = new ByteArrayOutputStream();
+                byte data[] = new byte[1024];
+                long total = 0;
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    output.write(data, 0, count);
+                }
+                input.close();
+                output.flush();
             }
-            input.close();
-            output.flush();
-
            /* this.xml = new String(output.toByteArray());
             Log.e("xml: ", xml);
             reader = new BufferedReader(new StringReader(xml));*/
