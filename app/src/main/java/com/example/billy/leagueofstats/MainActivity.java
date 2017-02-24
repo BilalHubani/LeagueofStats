d5c690b19bfc9db2433a3070a4f3acf0c769788f
BilalHubani <bilal.hubani@gmail.com>
2017-02-24 19:12:52 +0100

    spinner done

--- app/src/main/java/com/example/billy/leagueofstats/MainActivity.java
+++ app/src/main/java/com/example/billy/leagueofstats/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.billy.leagueofstats;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
